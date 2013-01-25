bebb8bc45e462ba053b61bcd2f44ac9dfc345d1f
Alex Ciminian <alex.ciminian@gmail.com>
2013-01-25 11:38:29 +0200

    refactoring

--- patterns.js
+++ patterns.js
@@ -2,42 +2,9 @@ var uglify = require('uglify-js');
 
 var isObjectLiteral = function (node) {
   return node.start.value === '{' && node.end.value === '}';
-}
+};
 
-var traverse = function (node) {
-  var result;
-  if (node.operator === '=' && node.left.property === 'version' 
-      && ~['module', 'exports'].indexOf(node.left.start.value)) {
-    // matching module.exports.version and exports.version
-    // TODO: also matches module.version which is not quite OK
-    return node.right.end.value;
-  } else if (node.operator === '=' && node.left.property === 'exports'
-    && isObjectLiteral(node.right)) {
-    result = extractVersion(node.right.properties);
-    if (result) return result;
-  } else if (node.start && node.value && node.start.value == 'return'
-      && isObjectLiteral(node.value)) {
-    result = extractVersion(node.value.properties);
-    if (result) return result;
-  } else if (node.length || (node.body && node.body.length)) {
-    // using every because we need
-    // to break out of the loop
-    if (node.body && node.body.length) {
-      node = node.body;
-    }
-    node.every(function (n) {
-      result = traverse(n);
-      return result ? false : true;
-    });
-    return result;
-  } else if (node.body) {
-    return traverse(node.body);
-  } else if (node.expression) {
-    return traverse(node.expression);
-  }
-}
-
-var extractVersion = function (properties) {
+var extractVersionProperty = function (properties) {
   var result;
   properties.every(function (prop) {
     if (prop.key === 'version') {
@@ -46,7 +13,73 @@ var extractVersion = function (properties) {
     }
   });
   return result;
+};
+
+var patterns = [];
+
+patterns.push(function (node) {
+  // matching module.exports.version = '...' and exports.version = '...'
+  // TODO: also matches module.version which is not quite OK
+  if (node.operator === '=' && node.left.property === 'version' 
+    && ~['module', 'exports'].indexOf(node.left.start.value)) {
+  return node.right.end.value;
+  }
+});
+
+patterns.push(function (node) {
+  // matching object literals from assignments to exports
+  if (node.operator === '=' && node.left.property === 'exports'
+    && isObjectLiteral(node.right)) {
+    result = extractVersionProperty(node.right.properties);
+    if (result) return result;
+  };
+});
+
+patterns.push(function (node) {
+  // matching return { [...], version: '...', [...] }
+  if (node.start && node.value && node.start.value == 'return'
+      && isObjectLiteral(node.value)) {
+    result = extractVersionProperty(node.value.properties);
+    if (result) return result;
+  }
+});
+
+patterns.match = function (node) {
+  var result;
+  // using every because we need
+  // to break out of the loop
+  patterns.every(function (pattern) {
+    result = pattern(node);
+    return result ? false : true;
+  });
+  return result;
 }
+
+
+var traverse = function (node) {
+  var result = patterns.match(node);
+  if (result) {
+    // if a pattern matched, we return what we found and exit
+    return result;
+  } else {
+    // otherwise, we go deeper in the tree
+    if (node.length || (node.body && node.body.length)) {
+      if (node.body && node.body.length) {
+        node = node.body;
+      }
+      node.every(function (n) {
+        result = traverse(n);
+        return result ? false : true;
+      });
+      return result;
+    } else if (node.body) {
+      return traverse(node.body);
+    } else if (node.expression) {
+      return traverse(node.expression);
+    }
+  }
+}
+
 
 var exports = module.exports = {};
 
