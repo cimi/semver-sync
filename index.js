0c3679a58d7aadca0392919e14ca01319bb19dee
Alex Ciminian <alex.ciminian@gmail.com>
2013-01-25 23:51:11 +0200

    added documentation, minor formatting fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var DEFAULT_SEPARATOR = '\n'
 
 var exports = module.exports = { name: 'semver-sync' };
 
-module.exports.version = '1.0.2';
+module.exports.version = '1.0.3';
 
 var getExtension = function (filename) {
   var parts = filename.split('.');
